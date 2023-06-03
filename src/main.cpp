3274515bf023850684f97b5cdf47cd5c4c1c641c
Gustice <jakob.ger@gmail.com>
2023-06-03 10:05:37 +0200

    feature: adding UART-interface in echo mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
-#include <stdio.h>
-#include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_netif.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_eth.h"
 #include "esp_event.h"
 #include "esp_log.h"
-#include "driver/gpio.h"
+#include "esp_netif.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "sdkconfig.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 static const char *TAG = "eth_example";
 
 extern "C" { // This switch allows the ROS C-implementation to find this main
 void app_main(void);
+}
+
+#define ECHO_TEST_TXD 4
+#define ECHO_TEST_RXD 36
+#define ECHO_TEST_RTS -1
+#define ECHO_TEST_CTS -1
+
+#define ECHO_UART_PORT_NUM 1
+#define ECHO_UART_BAUD_RATE 115200
+#define ECHO_TASK_STACK_SIZE 4098
+
+#define BUF_SIZE (1028)
+
+static void echo_task(void *arg) {
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = ECHO_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+    int intr_alloc_flags = 0;
+
+    ESP_ERROR_CHECK(uart_driver_install((uart_port_t)ECHO_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL,
+                                        intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(ECHO_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(ECHO_UART_PORT_NUM, ECHO_TEST_TXD, ECHO_TEST_RXD, ECHO_TEST_RTS,
+                                 ECHO_TEST_CTS));
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *)malloc(BUF_SIZE);
+
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(ECHO_UART_PORT_NUM, data, (BUF_SIZE - 1), 20 / portTICK_RATE_MS);
+        // Write data back to the UART
+        uart_write_bytes(ECHO_UART_PORT_NUM, (const char *)data, len);
+        if (len) {
+            data[len] = '\0';
+            ESP_LOGI(TAG, "Recv str: %s", (char *)data);
+        }
+    }
 }
 
 static int cnt;
@@ -21,12 +68,11 @@ static int cnt;
 #define CONFIG_EXAMPLE_ETH_PHY_RST_GPIO -1
 #define CONFIG_EXAMPLE_ETH_MDC_GPIO 23
 #define CONFIG_EXAMPLE_ETH_MDIO_GPIO 18
-#define	PIN_PHY_POWER	12
+#define PIN_PHY_POWER 12
 
 /** Event handler for Ethernet events */
-static void eth_event_handler(void *arg, esp_event_base_t event_base,
-                              int32_t event_id, void *event_data)
-{
+static void eth_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id,
+                              void *event_data) {
     uint8_t mac_addr[6] = {0};
     /* we can get the ethernet driver handle from event data */
     esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
@@ -35,8 +81,8 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
     case ETHERNET_EVENT_CONNECTED:
         esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
         ESP_LOGI(TAG, "Ethernet Link Up");
-        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
-                 mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x", mac_addr[0], mac_addr[1],
+                 mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
         break;
     case ETHERNET_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "Ethernet Link Down");
@@ -53,10 +99,9 @@ static void eth_event_handler(void *arg, esp_event_base_t event_base,
 }
 
 /** Event handler for IP_EVENT_ETH_GOT_IP */
-static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
-                                 int32_t event_id, void *event_data)
-{
-    ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+static void got_ip_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id,
+                                 void *event_data) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
     const esp_netif_ip_info_t *ip_info = &event->ip_info;
 
     ESP_LOGI(TAG, "Ethernet Got IP Address");
@@ -67,8 +112,7 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     ESP_LOGI(TAG, "~~~~~~~~~~~");
 }
 
-void app_main(void)
-{
+void app_main(void) {
     // Initialize TCP/IP network interface (should be called only once in application)
     ESP_ERROR_CHECK(esp_netif_init());
     // Create default event loop that running in background
@@ -85,7 +129,7 @@ void app_main(void)
     phy_config.phy_addr = CONFIG_EXAMPLE_ETH_PHY_ADDR;
     phy_config.reset_gpio_num = CONFIG_EXAMPLE_ETH_PHY_RST_GPIO;
     gpio_pad_select_gpio((gpio_num_t)PIN_PHY_POWER);
-    gpio_set_direction((gpio_num_t)PIN_PHY_POWER,GPIO_MODE_OUTPUT);
+    gpio_set_direction((gpio_num_t)PIN_PHY_POWER, GPIO_MODE_OUTPUT);
     gpio_set_level((gpio_num_t)PIN_PHY_POWER, 1);
     vTaskDelay(pdMS_TO_TICKS(10));
     mac_config.smi_mdc_gpio_num = CONFIG_EXAMPLE_ETH_MDC_GPIO;
@@ -99,13 +143,17 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handle)));
 
     // Register user defined event handers
-    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
-    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+    ESP_ERROR_CHECK(
+        esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
     /* start Ethernet driver state machine */
     ESP_ERROR_CHECK(esp_eth_start(eth_handle));
 
+    xTaskCreate(echo_task, "uart_echo_task", ECHO_TASK_STACK_SIZE, NULL, 10, NULL);
+
     while (true) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("Tick %d\n", cnt++);
+        std::cout << "Cnt " << cnt++ << "\n";
     }
 }
